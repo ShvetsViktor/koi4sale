6d2c57bb7f6c9d07bbdc4debb313265043766242
Viktor <shvetsviktor89@gmail.com>
2025-09-08 12:09:14 +0100

    test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
                                 href="/#"
                                 className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-orange-700"
                             >
-                                Contact You
+                                Contact Some of US
                             </a>
                         </div>
                     </div>
