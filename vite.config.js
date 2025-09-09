593317c8d4514d2b255e1d79387098d32a81d94a
Viktor <shvetsviktor89@gmail.com>
2025-09-09 17:52:20 +0100

    vite cpnfig base edited

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/koi4sale/',
+  base: '/',
   resolve: {
     alias: {
       tailgrids: path.resolve(__dirname, 'node_modules/tailgrids/src'),
